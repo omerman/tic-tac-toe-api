8e6c3ab149be55f30a164b0918ffdb72739089a6
Omer Prizner <omer.prizner@gmail.com>
2021-09-27 21:41:18 +0300

    types

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export enum Square {
+  Empty,
+  X,
+  O,
+}
+
+export enum GameStatus {
+  AwaitingPlayer2,
+  InProgress,
+  Player1Wins,
+  Player2Wins,
+  Draw,
+}
+
+export interface User {
+  id: number;
+}
+
+export type Board = Square[][];
+
+export interface Game {
+  id: number;
+  board: Board;
+  player1: User;
+  player2?: User;
+}
